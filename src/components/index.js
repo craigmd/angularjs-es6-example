842ce85943432695c5cadc7878a5580f49828042
craigmd <craigmichaeldickson@gmail.com>
2017-04-28 09:21:23 -0400

    Allow the user to follow another user

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
 import angular from 'angular';
 import ListErrors from './list-errors.component'
+import FollowBtn from './buttons/follow-btn.component'
 import ShowAuthed from './show-authed.directive'
 
 let componentsModule = angular.module('app.components', []);
 
 // Components
 componentsModule.component('listErrors', ListErrors);
+componentsModule.component('followBtn', FollowBtn);
 
 // Directives
 componentsModule.directive('showAuthed', ShowAuthed);
